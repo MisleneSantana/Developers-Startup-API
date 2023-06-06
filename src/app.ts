4c9ae01ec149c08a8573070a140be1eb80f7d00c
MisleneSantana <contact.mislene@gmail.com>
2023-06-06 21:45:05 +0300

    create DER and queries create tables

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import express, { Application } from "express";
-import "dotenv/config";
+import 'express-async-errors';
+import express, { Application } from 'express';
+import handleError from './middlewares/handleErrors.middleware';
 
 const app: Application = express();
+app.use(express.json());
+
+app.use('/'); //Arquivo de rotas
+
+app.use(handleError); //Deve ser o último dentro do app
 
 export default app;
